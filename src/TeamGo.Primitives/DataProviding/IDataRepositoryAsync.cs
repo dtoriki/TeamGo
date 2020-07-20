a08b5f53ab84e3b4c9c0ed85e4d668edbf5ac1b8
Лоскутов Михаил <loskutov.m@konsom.ru>
2020-07-20 19:54:53 +0500

    refactor: ITableEntity => IDataEntity

--- src/TeamGo.Primitives/DataProviding/IDataRepositoryAsync.cs
+++ src/TeamGo.Primitives/DataProviding/IDataRepositoryAsync.cs
@@ -19,57 +19,57 @@ namespace TeamGo.Primitives.DataProviding
         /// <summary>
         /// Добавляет сущность в структуру данных
         /// </summary>
-        /// <typeparam name="TEntity">Тип сущности <see cref="ITableEntity{TId}"/></typeparam>
+        /// <typeparam name="TEntity">Тип сущности <see cref="IDataEntity{TId}"/></typeparam>
         /// <typeparam name="TId">Тип уникального идентификатора сущности</typeparam>
         /// <param name="entity">Сущность для добавления</param>
         /// <returns>Присвоенный уникальный идентификатор</returns>
         Task<TId> CreateEntityAsync<TEntity, TId>(TEntity entity)
-            where TEntity : class, ITableEntity<TId>;
+            where TEntity : class, IDataEntity<TId>;
         /// <summary>
         /// Добавляет сущность в структуру данных
         /// </summary>
-        /// <typeparam name="TEntity">Тип сущности <see cref="ITableEntity{TId}"/></typeparam>
+        /// <typeparam name="TEntity">Тип сущности <see cref="IDataEntity{TId}"/></typeparam>
         /// <typeparam name="TId">Тип уникального идентификатора сущности</typeparam>
         /// <param name="entityOptions">Настройки добавляемой сущности</param>
         /// <returns>Присвоенный уникальный идентификатор</returns>
         Task<TId> CreateEntityAsync<TEntity, TId>(Action<TEntity> entityOptions)
-            where TEntity : class, ITableEntity<TId>, new();
+            where TEntity : class, IDataEntity<TId>, new();
         /// <summary>
         /// Читает сущность из структуры данных
         /// </summary>
-        /// <typeparam name="TEntity">Тип сущности <see cref="ITableEntity{TId}"/></typeparam>
+        /// <typeparam name="TEntity">Тип сущности <see cref="IDataEntity{TId}"/></typeparam>
         /// <typeparam name="TId">Тип уникального идентификатора сущности</typeparam>
         /// <param name="id">Уникальный идентификатор сущности</param>
         /// <returns>Сущность</returns>
         Task<TEntity> ReadEntityAsync<TEntity, TId>(TId id)
-            where TEntity : class, ITableEntity<TId>;
+            where TEntity : class, IDataEntity<TId>;
         /// <summary>
         /// Читает множество сущностей из структуры данных.
         /// </summary>
-        /// <typeparam name="TEntity">Тип сущности <see cref="ITableEntity{TId}"/></typeparam>
+        /// <typeparam name="TEntity">Тип сущности <see cref="IDataEntity{TId}"/></typeparam>
         /// <typeparam name="TId">Тип уникального идентификатора сущности</typeparam>
         /// <param name="predicate">Условие выбора сущностей</param>
         /// <returns>Сущности</returns>
         Task<IEnumerable<TEntity>> ReadEntitiesAsync<TEntity, TId>(Func<TEntity, bool> predicate)
-            where TEntity : class, ITableEntity<TId>;
+            where TEntity : class, IDataEntity<TId>;
         /// <summary>
         /// Читает множество сущностей из структуры данных
         /// </summary>
-        /// <typeparam name="TEntity">Тип сущности <see cref="ITableEntity{TId}"/></typeparam>
+        /// <typeparam name="TEntity">Тип сущности <see cref="IDataEntity{TId}"/></typeparam>
         /// <typeparam name="TId">Тип уникального идентификатора сущности</typeparam>
         /// <param name="id">Уникальный идентификатор сущности</param>
         /// <param name="updateAction">Действия обновления</param>
         Task UpdateEntity<TEntity, TId>(TId id, Action<TEntity> updateAction)
-            where TEntity : class, ITableEntity<TId>;
+            where TEntity : class, IDataEntity<TId>;
         /// <summary>
         /// Читает множество сущностей из структуры данных
         /// </summary>
-        /// <typeparam name="TEntity">Тип сущности <see cref="ITableEntity{TId}"/></typeparam>
+        /// <typeparam name="TEntity">Тип сущности <see cref="IDataEntity{TId}"/></typeparam>
         /// <typeparam name="TId">Тип уникального идентификатора сущности</typeparam>
         /// <param name="id">Уникальный идентификатор сущности</param>
         /// <param name="newEntity">Сущность на замену</param>
         Task UpdateEntity<TEntity, TId>(TId id, TEntity newEntity)
-            where TEntity : class, ITableEntity<TId>;
+            where TEntity : class, IDataEntity<TId>;
         /// <summary>
         /// Удаляет сущность из структуры данных
         /// </summary>
