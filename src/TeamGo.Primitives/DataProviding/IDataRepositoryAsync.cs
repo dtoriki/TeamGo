9ddae8d67d32a8de38eded12fe971927d48726f4
Лоскутов Михаил <loskutov.m@konsom.ru>
2020-07-20 16:20:06 +0500

    refatcor: Переименовал неймспейс из DataContext в DataProviding

--- src/TeamGo.Primitives/DataProviding/IDataRepositoryAsync.cs
+++ src/TeamGo.Primitives/DataProviding/IDataRepositoryAsync.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace TeamGo.Primitives.DataContext
+namespace TeamGo.Primitives.DataProviding
 {
     /// <summary>
     /// Представляет собой репозиторий поставщика данных.
