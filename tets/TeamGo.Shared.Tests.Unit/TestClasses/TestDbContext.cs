6820d204e7066b45239a56bc6e584492c473b1a9
Mikhail Loskutov <ms.toriki@mail.ru>
2020-07-22 23:25:36 +0500

    tests: Add some tests for DBContextRepository (need more)

---
+++ tets/TeamGo.Shared.Tests.Unit/TestClasses/TestDbContext.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TeamGo.Shared.Abstracts.DataProviding;
+
+namespace TeamGo.Shared.Tests.Unit.TestClasses
+{
+    public class TestDbContext : DbContext, IDataProvider
+    {
+        public DbSet<TestEntity> TestTable { get; set; }
+
+#pragma warning disable CS8618 // Поле, не допускающее значение NULL, не инициализировано. Рекомендуется объявить его как допускающее значение NULL.
+        public TestDbContext(DbContextOptions<TestDbContext> opt) : base (opt)
+#pragma warning restore CS8618 // Поле, не допускающее значение NULL, не инициализировано. Рекомендуется объявить его как допускающее значение NULL.
+        {
+
+        }
+
+        public void Save()
+        {
+            SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await SaveChangesAsync();
+        }
+    }
+}
